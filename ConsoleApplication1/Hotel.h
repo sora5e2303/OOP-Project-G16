8cdf20aafb77b48a7f232c009a247aa19c89323d
sora5e2303 <128419972+sora5e2303@users.noreply.github.com>
2023-03-29 14:54:28 -0400

    .

---
+++ ConsoleApplication1/Hotel.h
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "Room.h"
+
+using namespace std;
+
+class Hotel {
+private:
+    string name;
+    string address;
+    string city;
+    string state;
+    string zipCode;
+    vector<Room*> rooms;
+
+public:
+    // Constructor
+    Hotel(string name, string address, string city, string state, string zipCode) {
+        this->name = name;
+        this->address = address;
+        this->city = city;
+        this->state = state;
+        this->zipCode = zipCode;
+    }
+
+    // Getters and Setters
+    string getName() const {
+        return name;
+    }
+
+    void setName(string name) {
+        this->name = name;
+    }
+
+    string getAddress() const {
+        return address;
+    }
+
+    void setAddress(string address) {
+        this->address = address;
+    }
+
+    string getCity() const {
+        return city;
+    }
+
+    void setCity(string city) {
+        this->city = city;
+    }
+
+    string getState() const {
+        return state;
+    }
+
+    void setState(string state) {
+        this->state = state;
+    }
+
+    string getZipCode() const {
+        return zipCode;
+    }
+
+    void setZipCode(string zipCode) {
+        this->zipCode = zipCode;
+    }
+
+    // Room Management
+    void addRoom(Room* room) {
+        rooms.push_back(room);
+    }
+
+    Room* getRoom(int roomNumber) const {
+        for (int i = 0; i < rooms.size(); i++) {
+            if (rooms[i]->getRoomNumber() == roomNumber) {
+                return rooms[i];
+            }
+        }
+        return NULL;
+    }
+
+    vector<Room*> getRooms() const {
+        return rooms;
+    }
+
+    // Display Hotel Information
+    void display() const {
+        cout << "Hotel Name: " << name << endl;
+        cout << "Address: " << address << ", " << city << ", " << state << " " << zipCode << endl;
+        cout << "Rooms: " << endl;
+        for (int i = 0; i < rooms.size(); i++) {
+            rooms[i]->display();
+        }
+    }
+
+    // Destructor
+    ~Hotel() {
+        for (int i = 0; i < rooms.size(); i++) {
+            delete rooms[i];
+        }
+    }
+};
