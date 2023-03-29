86feccec26158def1c661d6105efe0daf5882593
mccuneo <128420816+mccuneo@users.noreply.github.com>
2023-03-29 16:06:41 -0400

    .

--- ConsoleApplication1/Reservation.cpp
+++ ConsoleApplication1/Reservation.cpp
@@ -1,61 +1,18 @@
 #include "Reservation.h"
-
+//Assingned values to variables
 Reservation::Reservation() {}
-
-Reservation::Reservation(Guest guest, Room room, Date startDate, Date endDate, int reservationID) {
-    this->guest = guest;
-    this->room = room;
-    this->startDate = startDate;
-    this->endDate = endDate;
-    this->reservationID = reservationID;
+{
+guest = " ";
+room = " ";
+startDate = 0;
+endDate = 0;
+reservationID= 0;
 }
 
-Guest Reservation::getGuest() {
-    return guest;
-}
-
-Room Reservation::getRoom() {
-    return room;
-}
-
-Date Reservation::getStartDate() {
-    return startDate;
-}
-
-Date Reservation::getEndDate() {
-    return endDate;
-}
-
-int Reservation::getReservationID() {
-    return reservationID;
-}
-
-int Reservation::getNumberOfDays() {
-    return endDate.getJulianDate() - startDate.getJulianDate();
-}
-
-double Reservation::getTotalPrice() {
-    int numberOfDays = getNumberOfDays();
-    double roomPrice = room.getPrice();
-    return numberOfDays * roomPrice;
-}
-
-void Reservation::setGuest(Guest guest) {
-    this->guest = guest;
-}
-
-void Reservation::setRoom(Room room) {
-    this->room = room;
-}
-
-void Reservation::setStartDate(Date startDate) {
-    this->startDate = startDate;
-}
-
-void Reservation::setEndDate(Date endDate) {
-    this->endDate = endDate;
-}
-
-void Reservation::setReservationID(int reservationID) {
-    this->reservationID = reservationID;
-}
+//Defining variables
+Reservation::Reservation(string g, string r, int s, int e, int i){
+    guest = g;
+    room = r;
+    startDate = s;
+    endDate = e;
+}reservationID = i; 
--- ConsoleApplication1/Reservation.h
+++ ConsoleApplication1/Reservation.h
@@ -1,40 +1,45 @@
 #include <iostream>
 #include <string>
-
 #include "Guest.h"
 #include "Room.h"
 #include "Date.h"
 
 using namespace std;
 
-class Reservation{
+class Reservation {
 //Class Structure
     private:
-        Guest guest;
-        Room room;
-        Date startDate;
-        Date endDate;
+        string guest;
+        string room;
+        int startDate;
+        int endDate;
         int reservationID;
 
     public:
         Reservation();
 //Overloaded Constructor
-        Reservation(Guest guest, Room room, Date startDate, Date endDate, int reservationID);
+        Reservation(string guest, string room, int startDate, int endDate, int reservationID);
 
-// Getters and Setters
-        Guest getGuest();
-        Room getRoom();
-        Date getStartDate();
-        Date getEndDate();
-        int getReservationID();
-        int getNumberOfDays();
-        double getTotalPrice();
-
-        void setGuest(Guest guest);
-        void setRoom(Room room);
-        void setStartDate(Date startDate);
-        void setEndDate(Date endDate);
-        void setReservationID(int reservationID);
+// Getters to return required values
+        string getguest() {return guest;};
+        string getroom(){return room;};
+        int getstartDate(){return startDate;};
+        int getendDate(){return endDate;};
+        int getreservationID(){return reservationID};
+//Setters to assign letters to variables
+        void setguest(string g) {g = guest;};
+        void setroom(string r){r= room;};
+        void setstartDate(int s){s= startDate;};
+        void setendDate(int e){e = endDate;};
+        void setreservationID(int i){i = reservationID};
+//Virtual void to print variables when code is run
+        virtual void print () {
+            cout << "Guest Name: " getguest()<< endl;
+            cout << "Room Number: " getroom()<< endl;
+            cout << "Start Date: " getstartDate()<<endl;
+            cout << " End Date: " getendDate() << endl;
+            cout << " Reservation ID: " getreservationID() << endl;
+        }
 };
 
 
