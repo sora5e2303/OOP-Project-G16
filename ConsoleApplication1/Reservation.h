f4fe03457e28c5f0ab4466163480f17d9ffa961d
sora5e2303 <128419972+sora5e2303@users.noreply.github.com>
2023-04-02 16:08:16 -0400

    .

--- ConsoleApplication1/Reservation.h
+++ ConsoleApplication1/Reservation.h
@@ -10,7 +10,6 @@ using namespace std;
 class Reservation {
 //Class Structure
     private:
-        string guest;
         string room;
         string startDate;
         string endDate;
@@ -22,14 +21,12 @@ class Reservation {
         Reservation(string guest, string room, int startDate, int endDate, int reservationID);
 
 // Getters to return required values
-        string getguest() {return guest;};
         string getroom(){return room;};
         string getstartDate(){return startDate;};
         string getendDate(){return endDate;};
         int getreservationID(){return reservationID};
 
 //Setters to assign letters to variables
-        void setguest(string g) {guest = g;};
         void setroom(string r){room = r;};
         void setstartDate(string s){startDate = s;};
         void setendDate(string e){endDate = e;};
@@ -37,11 +34,11 @@ class Reservation {
 
 //Virtual void to print variables when code is run
         virtual void print () {
-            cout << "Guest Name: " getguest()<< endl;
-            cout << "Room Number: " getroom()<< endl;
-            cout << "Start Date: " getstartDate()<<endl;
-            cout << "End Date: " getendDate() << endl;
-            cout << "Reservation ID: " getreservationID() << endl;
+            cout << "Guest Name: " << GetF() << GetL() << endl;
+            cout << "Room Number: " << getroom()<< endl;
+            cout << "Start Date: " << getstartDate()<<endl;
+            cout << "End Date: " << getendDate() << endl;
+            cout << "Reservation ID: " << getreservationID() << endl;
         }
 };
 
