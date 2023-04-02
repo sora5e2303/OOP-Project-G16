7d49ebc985fa91f32c0b36e787c1fbf6839f7d2c
sora5e2303 <128419972+sora5e2303@users.noreply.github.com>
2023-04-02 15:50:32 -0400

    .

--- ConsoleApplication1/Person/Guest.cpp
+++ ConsoleApplication1/Person/Guest.cpp
@@ -1,19 +1,52 @@
 #include "Guest.h"
 
-Guest::Guest() : Person(){
+Guest::Guest() {
     g_id = 0;
     m_first = " ";
     m_last = " ";
     m_address = " ";
-    m_phone = " ";
-    m_email = " "; 
+    m_phone = 0;
+    m_email = " ";
 }
 
-Guest::Guest(int i, string f, string l, string a, string p, string e){
+Guest::Guest(int i, string f, string l, string a, int p, string e) {
     g_id = i;
     m_first = f;
     m_last = l;
     m_address = a;
     m_phone = p;
-    m_email = e; 
+    m_email = e;
+}
+void Guest::WriteToFile() {
+
+    ofstream Writer;
+    Writer.open("GuestData.txt", ios::app);
+
+    Writer << g_id << "   " << m_first << "   " << m_last << "   " << m_address << "   " << m_phone << "   " << m_email << endl;
+    
+}
+
+bool Guest::InFile(const string& GD) {
+
+    ifstream file(GD);
+    int num;
+    while (file >> num) {
+        if (num == g_id) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void Guest::Print() {
+
+    while (!InFile("GuestData.txt")) {
+        cin.ignore();
+        cout << "Please enter a valid ID: ";
+        cin >> g_id;
+    }
+    if (InFile("GuestData.txt")) {
+        cout << "Welcome back!\n\n";
+    }
+
 }
--- ConsoleApplication1/Person/Guest.h
+++ ConsoleApplication1/Person/Guest.h
@@ -1,34 +1,39 @@
+#pragma once
 #include <iostream>
 #include <string>
 #include <fstream>
 using namespace std;
 
-class Person {
+class Guest {
 
 protected:
     string m_first;
     string m_last;
     string m_address;
-    string m_phone;
+    int m_phone;
     string m_email;
     int g_id;
 
 public:
 
-Person();
-Person(string f, string l, string a,string p, string e );
+    Guest();
+    Guest(int i, string f, string l, string a, int p, string e);
 
-string GetF(){return m_first;};
-string GetL(){return m_last;};
-string GetP(){return m_phone;};
-string GetA(){return m_address;};
-string GetE(){return m_email;};
-int Getid(){return 100000 + (rand() % 900001);};
+    string GetF() { return m_first; };
+    string GetL() { return m_last; };
+    int GetP() { return m_phone; };
+    string GetA() { return m_address; };
+    string GetE() { return m_email; };
+    int Getid() { return 100000 + (rand() % 900001); };
 
-void SetFirst(string f){m_first = f;};
-void SetLast(string l){m_last = l;};
-void SetAdd(string a){m_address = a;};
-void SetP(string p){m_phone = p;};
-void SetE(string e){m_email = e;};
-void Setid(){g_id = Getid();};
+    void SetFirst(string f) { m_first = f; };
+    void SetLast(string l) { m_last = l; };
+    void SetAdd(string a) { m_address = a; };
+    void SetP(int p) { m_phone = p; };
+    void SetE(string e) { m_email = e; };
+    void Setid(int i) { g_id = i; };
+
+    void WriteToFile();
+    bool InFile(const string& GD);
+    void Print();
 };
