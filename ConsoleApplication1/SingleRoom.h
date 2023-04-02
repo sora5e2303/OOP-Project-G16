6d7ca77e18cf9e04aed8fe0f5b354fa5b4c1b25d
elgheril <128635323+elgheril@users.noreply.github.com>
2023-04-02 12:53:46 -0400

    .

--- ConsoleApplication1/SingleRoom.h
+++ ConsoleApplication1/SingleRoom.h
@@ -1,24 +1,30 @@
 #include <string>
 #include <iostream>
+using namespace std;
 
-class SingleRoom: public Room;
 class SingleRoom {
     private: 
-    int m_maxcapguestone;
+        int m_maxcapguestone;
 
     public:
-SingleRoom();
-//Parameterized Constructor
-SingleRoom (int o) {
+        SingleRoom() {
+            m_maxcapguestone = 1;
+        }
 
-    m_maxcapguestone = o
-}
+        SingleRoom(int o) {
+            m_maxcapguestone = o;
+        }
 
-//Getters and Setters
-int Getmaxcapguestone() = {return m_maxcapguestone;}; 
-void Setmaxcapguestone(int o) {o = m_maxcapguestone;}; 
- void printmaxcapguestone() {
-    cout << "The single room capacity is one guest" << Getmaxcapguestone() << endl;
- }
-}
+        int Getmaxcapguestone() {
+            return m_maxcapguestone;
+        }
+        void Setmaxcapguestone(int o) {
+            m_maxcapguestone = o;
+        }
+
+        void printmaxcapguestone() {
+            cout << "The single room capacity is one guest" << Getmaxcapguestone() << endl;
+        }
+};
+
 
