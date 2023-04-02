417a9a5fe5bca53aba0b8d2b9be44b4f6eb42eff
elgheril <128635323+elgheril@users.noreply.github.com>
2023-04-02 12:27:49 -0400

    .

--- ConsoleApplication1/DoubleRoom.cpp
+++ ConsoleApplication1/DoubleRoom.cpp
@@ -4,16 +4,26 @@ const double DoubleRoom::BASE_PRICE = 100.0;
 const double DoubleRoom::EXTRA_PERSON_PRICE = 20.0;
 
 DoubleRoom::DoubleRoom(int roomNumber, int floor, int capacity, bool isAvailable, int numOccupants)
-    : Room(roomNumber, floor, capacity, isAvailable), numOccupants(numOccupants) {}
+{
+    int roomNumber = roomNumber;
+    int floor = floor;
+    int capacity = capacity;
+    bool isAvailable = isAvailable;
+    int numOccupants = numOccupants;
+}
 
-int DoubleRoom::getNumOccupants() {
+int DoubleRoom::getNumOccupants()
+{
     return numOccupants;
 }
 
-double DoubleRoom::calculatePrice() {
+double DoubleRoom::calculatePrice()
+{
     double price = BASE_PRICE;
-    if (numOccupants > 2) {
+    if (numOccupants > 2)
+    {
         price += (numOccupants - 2) * EXTRA_PERSON_PRICE;
     }
     return price;
 }
+
